d9751230cdc59fcda478d34badd318008fc9237f
BB <bb@bbohling-mac08.home>
2013-09-20 22:26:25 -0700

    Basics are working

--- PosterousToMarkdown.js
+++ PosterousToMarkdown.js
@@ -3,48 +3,79 @@ var mustache = require('mustache');
 var md = require('html-md')
 var moment = require('moment');
 
-var file = "/Users/Shared/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}/2009.09.01 Pix/post.json";
+//var file = "/Users/Shared/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}/2009.09.01 Pix/post.json";
+//var file = "/Users/Shared/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}/2010.10.30 Maui Photos/post.json";
+var sourceDirectory = '/Users/Shared/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}';
 
-fs.readFile(file, 'utf8', function (err, data) {
+
+fs.readdir(sourceDirectory, function (err, files) {
   if (err) {
-    console.log('Error: ' + err);
+    console.log('> ERROR: could not read directory.');
     return;
   }
-  
-  console.log("=== Reading file");
+  var ctr = 1;
+  files.forEach(function (fileObject) {
+    var path = sourceDirectory + '/' + fileObject;
+    fs.stat(path, function (err, stat) {
+      if (stat && stat.isDirectory()) {
+        var postPath = path + '/post.json';
+        fs.stat(postPath, function (err, stat) {
+          if (stat && stat.isFile()) {
+            console.log('=== DoIt');
+            doIt(postPath);
+          }
+        });  
+      }
+    });
+  });
 
-  data = JSON.parse(data);
- 
-  var tmpDate = moment(data.display_date);
-  var slug = data.slug;
-  var postTitle = data.title;
-  var publish = !data.draft;
-  var tags = data.tags;
-  var content = md(data.body_full);
-
-  // Get date parts for storing images
-  var dt = new Date(tmpDate);
-  var year = dt.getFullYear();
-  var month = dt.getMonth() + 1;
-  var monthDay = dt.getDate(); 
-  var postDate = moment(data.display_date).format("YYYY-MM-DD HH:mm");
-
-  post = {
-    date: postDate,
-    title: postTitle,
-    publish: publish,
-    tags: tags,
-    content: content
-  };
-
-  console.log('valid date? ' + moment(data.display_date).isValid());
-  console.log('date: ' + postDate);
-  console.log('title: ' + postTitle);
-  console.log('publish: ' + publish);
-
-  persistToFile(post);
-  
 });
+
+function doIt(file) {
+
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      console.log('Error: ' + err);
+      return;
+    }
+    
+    console.log("Reading file: " + file);
+
+    data = JSON.parse(data);
+   
+    var tmpDate = moment(data.display_date);
+    var slug = data.slug;
+    var postTitle = data.title;
+    var publish = !data.draft;
+    var tags = data.tags;
+    var content = md(data.body_full);
+
+    // Get date parts for storing images
+    var dt = new Date(tmpDate);
+    var year = dt.getFullYear();
+    var month = dt.getMonth() + 1;
+    var monthDay = dt.getDate(); 
+    var postDate = moment(data.display_date).format("YYYY-MM-DD HH:mm");
+    var fileName = moment(data.display_date).format("YYYY-MM-DD") + '-' + slug;
+
+    post = {
+      date: postDate,
+      title: postTitle,
+      publish: publish,
+      tags: tags,
+      content: content,
+      fileName: fileName
+    };
+
+    console.log('valid date? ' + moment(data.display_date).isValid());
+    console.log('date: ' + postDate);
+    console.log('title: ' + postTitle);
+    console.log('publish: ' + publish);
+
+    persistToFile(post);
+
+  });
+}
 
 function persistToFile(post) {
   var templateFile = "postTemplate.md";
@@ -55,17 +86,17 @@ function persistToFile(post) {
     }
     var postText = mustache.to_html(data, post);
 
-    var savePostAs = "test.md";
-    fs.writeFile(savePostAs, postText, function (err, data) {
+    var savePostAs = "output/" + post.fileName + ".md";
+    fs.writeFile(savePostAs, postText, 'utf8', function (err, data) {
       if (err) {
         console.log('Save post as markdown: ' + err);
         return;
-      } else {
-        console.log('Save post as markdown: done');
       }
+      console.log('Save post as markdown: done');
     });
   });
 }
+
 
 
 function copyFile(source, target, cb) {
