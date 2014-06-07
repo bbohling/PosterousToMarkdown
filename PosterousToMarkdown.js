63e0d853f985b0bd4c876088994bf63a185c0fe5
Brandon Bohling <brandon.bohling@intel.com>
2014-06-07 07:58:55 -0700

    back to a previous commit before all async copying

--- PosterousToMarkdown.js
+++ PosterousToMarkdown.js
@@ -1,6 +1,4 @@
 var fs = require("fs");
-var path = require("path");
-var mkdirp = require('mkdirp');
 var mustache = require('mustache');
 var md = require('html-md')
 var moment = require('moment');
@@ -8,59 +6,40 @@ var moment = require('moment');
 //var file = "/Users/Shared/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}/2009.09.01 Pix/post.json";
 //var file = "/Users/Shared/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}/2010.10.30 Maui Photos/post.json";
 var sourceDirectory = '/Users/bbohling/Code/github/PosterousToMarkdown/data/Posterous - bbohling.com/2012.09.07-13.16.02 PosterousBackup - bohling@gmail.com/Brandon Bohling {Again}';
-var templateFile = "postTemplate.md";
-var postYear;
-var postMonth;
 
-function logit(msg, type) {
-  var prefix = '';
-  var type = '';
-  if (type = 'error') {
-    prefix = '> ERROR: ';
-  }
-  console.log(prefix + msg);
-}
-
-readDirectory(sourceDirectory, function (err, files) {
+fs.readdir(sourceDirectory, function (err, files) {
   if (err) {
-    logit('Bad start!', 'error');
+    console.log('> ERROR: could not read directory.');
     return;
   }
+  var ctr = 1;
   files.forEach(function (fileObject) {
-    var objectPath = sourceDirectory + '/' + fileObject;
-    fsCheck(objectPath, function (err, stat) {
-      if (err) {
-        logit('Sibling error: ' + objectPath, 'error');
-      }
+    var path = sourceDirectory + '/' + fileObject;
+    fs.stat(path, function (err, stat) {
       if (stat && stat.isDirectory()) {
-        var postPath = objectPath + '/post.json';
-        fsCheck(postPath, function (err, stat) {
-          if (err) {
-            logit('Could not access post.json: ' + postPath);
-            return;
-          }
+        var postPath = path + '/post.json';
+        fs.stat(postPath, function (err, stat) {
           if (stat && stat.isFile()) {
-            readAndSavePost(postPath, path);
-            copyImages(objectPath);
+            doIt(postPath);
           }
         });  
       }
     });
-       
   });
-})
 
+});
 
+function doIt(file) {
 
-function readAndSavePost(postPath) {
-  fsReadFile(postPath, function (err, content) {
+  fs.readFile(file, 'utf8', function (err, data) {
     if (err) {
-      console.log('DoIt error: ' + err);
+      console.log('Error: ' + err);
       return;
     }
-    var data = JSON.parse(content);
 
-    var tmpDate = moment(data.display_date);
+    data = JSON.parse(data);
+   
+    var tmpDate = moment(data.display_date, "YYYY/MM/DD HH:mm:SS Z"); //2012/08/03 17:53:29 -0700
     var slug = data.slug;
     var postTitle = data.title;
     var publish = !data.draft;
@@ -70,12 +49,10 @@ function readAndSavePost(postPath) {
     // Get date parts for storing images
     var dt = new Date(tmpDate);
     var year = dt.getFullYear();
-    postYear = year;
     var month = dt.getMonth() + 1;
-    postMonth = month;
     var monthDay = dt.getDate(); 
-    var postDate = moment(data.display_date).format("YYYY-MM-DD HH:mm");
-    var fileName = moment(data.display_date).format("YYYY-MM-DD") + '-' + slug;
+    var postDate = tmpDate.format("YYYY-MM-DD HH:mm");
+    var fileName = tmpDate.format("YYYY-MM-DD") + '-' + slug;
 
     post = {
       date: postDate,
@@ -86,104 +63,32 @@ function readAndSavePost(postPath) {
       fileName: fileName
     };
 
-    fsReadFile(templateFile, function (err, data) {
+    persistToFile(post);
+
+  });
+}
+
+function persistToFile(post) {
+  var templateFile = "postTemplate.md";
+  fs.readFile(templateFile, 'utf8', function (err, data) {
+    if (err) {
+      console.log('Error: ' + err);
+      return;
+    }
+    var postText = mustache.to_html(data, post);
+
+    var savePostAs = "output/" + post.fileName + ".md";
+    fs.writeFile(savePostAs, postText, 'utf8', function (err, data) {
       if (err) {
-        console.log('Error: ' + err);
+        console.log('> ERROR - Save post as markdown: ' + err);
         return;
       }
-      var postText = mustache.to_html(data, post);
-      var savePostAs = "output/" + post.fileName + ".md";
-      fsSaveFile(savePostAs, postText);
+      // console.log('Save post as markdown: done');
     });
   });
 }
 
-function copyImages(imagePath) {
-  var imagesFolder = imagePath + '/images';
 
-  fsCheck(imagesFolder, function (err, stat) {
-    if (err) {
-      logit('copyImages: ' + imagesFolder, 'error');
-      return;
-    }
-    if (stat && stat.isDirectory()) {
-      readDirectory(imagesFolder, function (err, images) {
-        if (err) {
-          logit('read image dir: ' + imagesFolder, 'error');
-          return;
-        }
-        // logit('i: ' + images);
-        images.forEach(function (image) {
-          var imagePath = imagesFolder + '/' + image;
-          fsCheck(imagePath, function (err, stat) {
-            if (err) {
-              logit('image: ' + imagePath, 'error');
-              return;
-            }
-            if (stat && stat.isFile()) {
-              if ((path.extname(imagePath) == 'jpg') || (path.extname(imagePath) == 'png') || (path.extname(imagePath) == 'gif')) {
-                var folderPath = 'uploads/' + postYear + '/' + postMonth;
-                logit('fp: ' + folderPath);
-                fsMakeDirectory(folderPath, function(err) { 
-                  if (err) {
-                    logit('mkdir: ' + folderPath, 'error');
-                    return;
-                  }
-                  copyFile(path, folderPath + '/' + image, cb);
-                });
-              }
-            }
-          });
-        });
-      });
-    } 
-  });
-}
-
-function readDirectory(directoryPath, callback) {
-  fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, files);
-  });
-}
-
-function fsCheck(fsPath, callback) {
-  fs.stat(fsPath, function (err, stat) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, stat);
-  });
-}
-
-function fsReadFile(file, callback) {
-  fs.readFile(file, 'utf8', function (err, fileContents) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, fileContents);
-  });
-}
-
-function fsSaveFile(postPath, postText, callback) {
-  fs.writeFile(postPath, postText, 'utf8', function (err) {
-    if (err) {
-      return callback(err);
-    }
-    console.log('Save file: done');
-  });
-}
-
-function fsMakeDirectory(folderPath, callback) {
-  mkdirp(folderPath, function(err) { 
-    if (err) {
-      return callback(err);
-    }
-    callback(err);
-  });
-}
 
 function copyFile(source, target, cb) {
   var cbCalled = false;
